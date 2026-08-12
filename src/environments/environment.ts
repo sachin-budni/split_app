5576a329a3ca358bcd44082a0b40e8472a135c66
sachin-budni <33023021+sachin-budni@users.noreply.github.com>
2026-08-12 17:06:44 +0530

    remove config
    Removed Firebase configuration from environment file.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,3 @@
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyDRIqdh1x7RFSuzT5KWREOlxoBSIzb3RVE",
-    authDomain: "split-aps.firebaseapp.com",
-    databaseURL: "https://split-aps-default-rtdb.firebaseio.com",
-    projectId: "split-aps",
-    storageBucket: "split-aps.firebasestorage.app",
-    messagingSenderId: "286477114082",
-    appId: "1:286477114082:web:3fd858cfa10e96d18adcbb",
-    measurementId: "G-JC7N58CBCG"
-  }
 };
